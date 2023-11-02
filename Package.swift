11e008b224a7cfbc2c55ec9767de1bace5eb6c33
TraxxasWyls <savinov.dsta@gmail.com>
2023-11-02 13:01:41 +0300

    Add spm support

--- Package.swift
+++ Package.swift
@@ -1,26 +1,29 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
 
 let package = Package(
     name: "Then",
     platforms: [
-        .macOS(.v10_10), 
-        .iOS(.v8), 
-        .tvOS(.v9), 
-        .watchOS(.v2)
+        .iOS(.v13),
+        .macOS(.v10_15),
+        .watchOS(.v4),
+        .tvOS(.v12)
     ],
     products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "Then", 
-            targets: ["Then"]
-        ),
+            name: "Then",
+            targets: ["Then"]),
     ],
     targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "Then"
-        ),
+            name: "Then"),
         .testTarget(
             name: "ThenTests",
-            dependencies: ["Then"]
-        ),
+            dependencies: ["Then"]),
     ]
 )
